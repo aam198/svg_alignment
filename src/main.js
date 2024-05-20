9c2345025bd69faf4a05b1c0255a0044d92e3131
aam198 <aam198@psu.edu>
2024-05-20 10:11:43 -0400

    code linting

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const showDialog = document.getElementById("number_1")
+const showDialog2 = document.getElementById("number_2")
 const dialog1 = document.getElementById('dialog_1')
+const dialog2 = document.getElementById('dialog_2')
 const closeButton = document.querySelector('dialog button');
 
 // Show dialog_1 modal
@@ -7,7 +9,12 @@ showDialog.addEventListener('click', () => {
   dialog1.showModal()
 });
 
+showDialog2.addEventListener('click', () => {
+  dialog2.showModal()
+});
+
 // Close dialog_1 modal
 closeButton.addEventListener('click', () =>{
   dialog1.close()
+  dialog2.close()
 });
