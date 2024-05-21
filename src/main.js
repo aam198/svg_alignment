c4530e0b366dd8bd12de6506cc0b2848def03dd5
aam198 <aam198@psu.edu>
2024-05-21 14:19:58 -0400

    forEach Close function added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+const dialog = document.querySelector('dialog')
 const showDialog = document.getElementById("number_1")
 const showDialog2 = document.getElementById("number_2")
 const dialog1 = document.getElementById('dialog_1')
 const dialog2 = document.getElementById('dialog_2')
-const closeButton = document.querySelector('dialog button');
+const closeButtons = document.querySelectorAll('dialog button');
 
 // Show dialog_1 modal
 showDialog.addEventListener('click', () => {
@@ -13,8 +14,16 @@ showDialog2.addEventListener('click', () => {
   dialog2.showModal()
 });
 
-// Close dialog_1 modal
-closeButton.addEventListener('click', () =>{
-  dialog1.close()
-  dialog2.close()
-});
+// Close dialog modal
+
+closeButtons.forEach((button) => {
+  console.log(button)
+  button.addEventListener('click', () => {
+    // Getting dialog based on being open
+    const dialogId = button.parentElement.id;
+    const dialog = document.getElementById(dialogId);
+    console.log(dialogId);
+    // Close the dialog
+    dialog.close();
+  })
+})
